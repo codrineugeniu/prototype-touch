c6b3727d8fb448a3fc069d1f2a5d04a5c6cc8133
Codrin Baleanu <codrin.baleanu@gmail.com>
2012-07-13 11:18:24 -0500

    Proposed changes

--- prototype.js
+++ prototype.js
@@ -11,6 +11,10 @@ function pointerX(event) {
     var docElement = document.documentElement,
      body = document.body || { scrollLeft: 0 };
 
+    if (event.changedTouches) return (event.changedTouches[0].clientX +
+      (docElement.scrollLeft || body.scrollLeft) -
+      (docElement.clientLeft || 0));
+
     return event.pageX || (event.clientX +
       (docElement.scrollLeft || body.scrollLeft) -
       (docElement.clientLeft || 0));
@@ -19,6 +23,10 @@ function pointerX(event) {
   function pointerY(event) {
     var docElement = document.documentElement,
      body = document.body || { scrollTop: 0 };
+
+    if (event.changedTouches) return (event.changedTouches[0].clientY +
+      (docElement.scrollLeft || body.scrollLeft) -
+      (docElement.clientLeft || 0));
 
     return  event.pageY || (event.clientY +
        (docElement.scrollTop || body.scrollTop) -
@@ -45,6 +53,10 @@ function pointerX(event) {
       Event.observe(document, "mouseup", this.eventMouseUp);
       Event.observe(document, "mousemove", this.eventMouseMove);
       Event.observe(document, "keypress", this.eventKeypress);
+
+      Event.observe(document, "touchstart", this.eventKeypress);
+      Event.observe(document, "touchmove", this.eventMouseMove);
+      Event.observe(document, "touchend", this.eventMouseUp);
     }
     this.drags.push(draggable);
   },
@@ -52,6 +64,10 @@ function pointerX(event) {
   unregister: function(draggable) {
     this.drags = this.drags.reject(function(d) { return d==draggable });
     if(this.drags.length == 0) {
+      Event.stopObserving(document, "mouseup", this.eventMouseUp);
+      Event.stopObserving(document, "mousemove", this.eventMouseMove);
+      Event.stopObserving(document, "keypress", this.eventKeypress);
+
       Event.stopObserving(document, "mouseup", this.eventMouseUp);
       Event.stopObserving(document, "mousemove", this.eventMouseMove);
       Event.stopObserving(document, "keypress", this.eventKeypress);
